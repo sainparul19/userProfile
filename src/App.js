25e0a71d29e39e236de10a2ddd60c249074e000f
parul sain <parulsain@paruls-Air.bbrouter>
2023-04-22 23:32:04 +0530

    correction made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Page from "./components/Page";
+import Post from "./components/Post";
 
 const App = () => {
   return (
     <div>
-      <Page />
+      <Post>
+        <Page />
+      </Post>
     </div>
   );
 };
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,70 +1,82 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./Page.css";
+import { ApiContext } from "./ContextProvider";
 import { MailOutlined } from "@ant-design/icons";
 import { PhoneOutlined } from "@ant-design/icons";
 import { GlobalOutlined } from "@ant-design/icons";
 import { HeartOutlined } from "@ant-design/icons";
 import { EditOutlined } from "@ant-design/icons";
 import { DeleteFilled } from "@ant-design/icons";
-import { person } from "./Post";
 
 const Page = () => {
+  const [blog, setBlog] = useContext(ApiContext);
+
+  useEffect(() => {
+    console.log(blog);
+  }, [blog]);
+
   return (
     <div className="container">
-      {person.map(({ name, avatar, email, phone, website }) => (
-        <div className="box">
+      {blog.map((res, id) => (
+        <div className="box" key={res.id}>
           <div className="innerBox">
             <div className="card">
               <div className="cardImage">
                 <img
                   style={{ width: 200, height: 200 }}
-                  src={avatar}
+                  src={`https://avatars.dicebear.com/v2/avataaars/${res.username}.svg?options[mood][]=happy`}
                   alt="avatar"
                 />
               </div>
             </div>
             <div className="cardBody">
-              <h3>{name}</h3>
+              <h3>{res.name}</h3>
               <div className="mainIcon">
                 <i className="icon">
                   <MailOutlined />
                 </i>
-                <p>{email}</p>
+                <p>{res.email}</p>
               </div>
               <div className="mainIcon">
                 <i className="icon">
                   <PhoneOutlined />{" "}
                 </i>
-                <p>{phone}</p>
+                <p>{res.phone}</p>
               </div>
               <div className="mainIcon">
                 <i className="icon">
                   <GlobalOutlined />
                 </i>
-                <p>{website}</p>
+                <p>{res.website}</p>
               </div>
             </div>
 
             <ul className="actions">
               <li>
                 <span>
-                  <i className="icon" style={{ color: "red" }}>
-                    <HeartOutlined />
-                  </i>
+                  <button>
+                    <i className="icon" style={{ color: "red" }}>
+                      <HeartOutlined />
+                    </i>
+                  </button>
                 </span>
               </li>
               <li>
                 <span>
-                  <i className="icon" style={{ color: "black" }}>
-                    <EditOutlined />
-                  </i>
+                  <button>
+                    <i className="icon" style={{ color: "black" }}>
+                      <EditOutlined fill="red" />
+                    </i>
+                  </button>
                 </span>
               </li>
               <li>
                 <span>
-                  <i className="icon" style={{ color: "black" }}>
-                    <DeleteFilled />
-                  </i>
+                  <button>
+                    <i className="icon" style={{ color: "black" }}>
+                      <DeleteFilled />
+                    </i>
+                  </button>
                 </span>
               </li>
             </ul>
